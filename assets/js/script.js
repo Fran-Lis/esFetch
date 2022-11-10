3d1b2cec49d60a9a44629eb6f3d68bab8881a448
Fran-Lis <106391193+Fran-Lis@users.noreply.github.com>
2022-11-10 11:52:11 +0100

    nuovo lavoro

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var elenco = [];
+var nome;
+var cognome;
+var email;
+var dati = document.getElementById('dati');
+window.addEventListener('DOMContentLoaded', init);
+function init() {
+
+    printData();
+
+}
+
+
+
+
+
+function printData() {
+    fetch('http://localhost:3000/utenti').then((response) => {
+        return response.json();
+    }).then((data) => {
+        elenco = data;
+        elenco.map(function(element) {
+dati.innerHTML +=`<tr><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td></tr>`;
+});
+    });
+}
